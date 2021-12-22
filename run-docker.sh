2cc7a2b34ec7b23273dad47e7cf01ae9ef58a79b
qiushao <qiushaocloud@126.com>
2021-12-23 00:08:20 +0800

    update sh file and env

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,13 @@
 CURR_DIR=$(cd "$(dirname "$0")"; pwd)
 
 if [ ! -f "$CURR_DIR/.env" ];then
-  echo "file .env is not exist"
+  echo "file $CURR_DIR/.env is not exist"
+  exit
+fi
+
+HALO_DATA_PATH=`grep HALO_DATA_PATH $CURR_DIR/.env|grep -v grep|awk -F '=' '{print $2}' | sed 's/ //g' | sed "s/'//g" | sed 's/\"//g'`
+if [ ! -f "$HALO_DATA_PATH/application.yaml" ];then
+  echo "file $HALO_DATA_PATH/application.yaml is not exist"
   exit
 fi
 
